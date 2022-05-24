7552ae25d5e0b6fb55b9d9728c7f7d7f29808a84
charles okunzo <okunzopeter01@gmail.com>
2022-05-24 22:07:44 +0300

    feature --add database relations

--- news/models.py
+++ news/models.py
@@ -5,3 +5,26 @@ class Editor(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   email = models.EmailField()
+
+
+  def __str__(self) -> str:
+      return self.first_name
+
+#ordering data when querying from db
+  class Meta:
+    ordering = ['first_name']
+
+class tags(models.Model):
+  name = models.CharField(max_length=30)
+
+
+  def __str__(self) -> str:
+      return self.name
+
+
+class Article(models.Model):
+  title = models.CharField(max_length=60)
+  post = models.TextField()
+  editor = models.ForeignKey(Editor)
+  tags = models.ManyToManyField(tags)
+  pub_date = models.DateTimeField(auto_now_add=True)
